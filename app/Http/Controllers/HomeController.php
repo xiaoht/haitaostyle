d71a1957f6d153b825ba7f4d59a2de76c31d5a3c
DESKTOP-EB9MSBQ\Admin <xht18721626070@gmail.com>
2018-03-29 13:25:07 +0800

    auth登录注册

---
+++ app/Http/Controllers/HomeController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class HomeController extends Controller
+{
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+
+    }
+
+    /**
+     * Show the application dashboard.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        flash('欢迎回来')->overlay();
+        return view('home');
+    }
+}
